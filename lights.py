34d9cf2bb883e3c2b0ec849d0e09803ea4b585d1
matt venn <matt@mattvenn.net>
2022-01-29 19:36:22 +0100

    presets

--- lights.py
+++ lights.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
+import pickle
 import sys, os
-from dmx import Colour, DMXLight, DMXInterface, DMXLight3Slot, DMXUniverse
+#from dmx import Colour, DMXLight, DMXInterface, DMXLight3Slot, DMXUniverse
+
+PRESET_FILE = "presets.pkl"
+
+class Preset(object):
+
+    def __init__(self, gui):
+        self.l_r = gui.left_r.value()
+        self.l_g = gui.left_g.value()
+        self.l_b = gui.left_b.value()
+        self.r_r = gui.right_r.value()
+        self.r_g = gui.right_g.value()
+        self.r_b = gui.right_b.value()
+
+        self.name = gui.preset_name.text()
+        self.link = gui.link.isChecked()
+
+    def __str__(self):
+        return("%s : %s. [%s %s %s] [%s %s %s]" % (self.name, self.link, self.l_r, self.l_g, self.l_b, self.r_r, self.r_g, self.r_b))
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -12,6 +31,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("DMX")
         self.setWindowIcon(QtGui.QIcon("icon.png"));
 
+        self.load_presets()
+
         self.left_r.valueChanged.connect(lambda: self.update_lights())
         self.left_g.valueChanged.connect(lambda: self.update_lights())
         self.left_b.valueChanged.connect(lambda: self.update_lights())
@@ -19,11 +40,17 @@ class MainWindow(QtWidgets.QMainWindow):
         self.right_g.valueChanged.connect(lambda: self.update_lights())
         self.right_b.valueChanged.connect(lambda: self.update_lights())
 
+
         # mem buttons
-        self.mem_z2a.clicked.connect(lambda: self.load("z2a"))
-        self.mem_yosys.clicked.connect(lambda: self.load("yosys"))
-        self.mem_red.clicked.connect(lambda: self.load("red"))
-        self.init_cam.clicked.connect(lambda: self.init_camera())
+        self.button_add.clicked.connect(self.add_preset)
+        self.button_delete.clicked.connect(self.delete_preset)
+        self.button_update.clicked.connect(self.update_preset)
+    
+        self.preset_name.textChanged.connect(self.preset_name_changed)
+        self.preset_list.currentIndexChanged.connect(self.preset_changed)
+        self.preset_list.activated.connect(self.preset_changed)
+
+#        self.button_delete.clicked.connect(lambda: self.load("yosys"))
 
         # dmx updates
         self.dmx_timer = QtCore.QTimer(self)
@@ -31,8 +58,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.dmx_timer.timeout.connect(self.update_dmx)
 
         # dmx interface setup with PyDMX
-        self.interface = DMXInterface("FT232R")
-        self.universe = DMXUniverse()
+#        self.interface = DMXInterface("FT232R")
+#        self.universe = DMXUniverse()
 
         # 2 x 24 channel lights chained, with first starting at address 1 and 2nd at 25
         # in 24 channel mode, have 8 segments with R, G and B.
@@ -41,56 +68,89 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.lights = []
         # Define DMX lights
-        for address in range(1, self.num_chan, 3):
+#        for address in range(1, self.num_chan, 3):
 #            print("setting up 1 x 3 channel light at address %d" % address)
-            light = DMXLight3Slot(address=address)
-            self.universe.add_light(light)
-            self.lights.append(light)
+#            light = DMXLight3Slot(address=address)
+#            self.universe.add_light(light)
+#            self.lights.append(light)
 
-    def init_camera(self):
-        print("init cam")
-        webcamdevice="/dev/video0"
-        cam_settings = {
-            'power_line_frequency': 1,
-            'exposure_auto': 1,
-            'gain' : 5,
-            'white_balance_temperature_auto': 0,
-            'white_balance_temperature' : 5500,
-            'exposure_absolute': 400,
-        }
-        for key, value in cam_settings.items():
-            print("setting %s to %d" % (key, value))
-            os.system("v4l2-ctl -d %s --set-ctrl=%s=%d" % (webcamdevice, key, value))
+    def preset_name_changed(self, text):
+        if text:
+            self.button_add.setEnabled(True)
+            self.button_update.setEnabled(True)
+        else:
+            self.button_add.setEnabled(False)
 
-    def load(self, mem):
-        if(mem == 'z2a'):
-            self.link.setChecked(0)
+    def preset_changed(self, index):
+        if index == -1:
+            print("no preset")
+            return
 
-            self.left_r.setValue(0);
-            self.left_g.setValue(0);
-            self.left_b.setValue(100);
+        print("change preset to %d" % index)
+        preset = self.presets[index]
+        self.button_update.setEnabled(False)
+        print(preset)
 
-            self.right_r.setValue(0);
-            self.right_g.setValue(117);
-            self.right_b.setValue(18);
+        self.link.setChecked(preset.link)
+        self.left_r.setValue(preset.l_r);
+        self.left_g.setValue(preset.l_g);
+        self.left_b.setValue(preset.l_b);
 
-        if(mem == 'yosys'):
-            self.link.setChecked(0)
-            self.left_r.setValue(180);
-            self.left_g.setValue(0);
-            self.left_b.setValue(44);
+        self.right_r.setValue(preset.r_r);
+        self.right_g.setValue(preset.r_g);
+        self.right_b.setValue(preset.r_b);
 
-            self.right_r.setValue(24);
-            self.right_g.setValue(0);
-            self.right_b.setValue(96);
+        self.preset_name.setText(preset.name)
 
-        if(mem == 'red'):
-            self.link.setChecked(1)
-            self.left_r.setValue(255);
-            self.left_g.setValue(0);
-            self.left_b.setValue(0);
+    def load_presets(self):
+        try:
+            with open(PRESET_FILE, 'rb') as fh:
+                self.presets = pickle.load(fh)
+                self.update_preset_combo_box()
+        except FileNotFoundError as e:
+            self.presets = []
+
+        if len(self.presets):
+            self.button_delete.setEnabled(True)
+            # force load of preset
+            self.preset_changed(0)
+
+    def update_preset_combo_box(self):
+        self.preset_list.clear()
+        for preset in self.presets:
+            self.preset_list.addItem(preset.name)
+
+    def save_presets(self):
+        print("save presets")
+        with open(PRESET_FILE, 'wb') as fh:
+            pickle.dump(self.presets, fh)
+        
+    def add_preset(self):
+        preset = Preset(self)
+        self.presets.append(preset)
+        self.preset_list.addItem(preset.name)
+        self.preset_list.setCurrentIndex(self.presets.index(preset))
+
+    def delete_preset(self):
+        index = self.preset_list.currentIndex()
+        print("removing %s at index %d" % (self.preset_list.currentText(), index))
+        del(self.presets[index])
+        self.preset_list.removeItem(index)
+        
+        if not len(self.presets):
+            self.button_delete.setEnabled(False)
+
+    def update_preset(self):
+        index = self.preset_list.currentIndex()
+        preset = Preset(self)
+        # overwrite
+        self.presets[index] = preset
+        print(self.presets[index])
+        self.update_preset_combo_box()
 
     def update_lights(self):
+        self.button_update.setEnabled(True)
+
         if self.link.isChecked():
             self.right_r.setValue(self.left_r.value())
             self.right_g.setValue(self.left_g.value())
@@ -105,23 +165,15 @@ class MainWindow(QtWidgets.QMainWindow):
             light.set_colour(Colour(R,G,B))
 
     def update_dmx(self):
-        self.interface.set_frame(self.universe.serialise())
-        self.interface.send_update()
+        pass
+#        self.interface.set_frame(self.universe.serialise())
+#        self.interface.send_update()
 
+
+    def closeEvent(self, event):
+        self.save_presets()
 
 if __name__ == '__main__':
-    """
-    parser = argparse.ArgumentParser(description="View Events")
-    parser.add_argument('--listing', help="load a Vallen CSV listing", action='store')
-    parser.add_argument('--db-file', help="load a Vallen DB (sqlite containing raw data)", action='store')
-    parser.add_argument('--channels', help="comma separated list of 4 channels: R1,R2,L1,L2", action='store', default="1,2,3,4")
-    parser.add_argument('--sensor-distances', help="comma separated list left and right sensor distances: RW,LW", action='store')
-    parser.add_argument('--calibrate', help="run calibrate at start", action="store_const", const=True)
-    parser.add_argument('--locate', help="run locate at start", action="store_const", const=True)
-    parser.add_argument('--tab', help="switch to this tab after startup (0 index)", action="store")
-    args = parser.parse_args()
-    """
-
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
     main.show()
